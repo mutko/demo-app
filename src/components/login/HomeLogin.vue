f6308d86ef4f97f64fad7438da039daaed04e6ca
Branko Bozovic <branko028@yahoo.com>
2019-06-13 11:34:33 +0200

    Work on router

---
+++ src/components/login/HomeLogin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Signin/>
+  </div>
+</template>
+
+<script>
+import Signin from "./Signin";
+
+export default {
+  name: "homelogin",
+  components: {
+    Signin
+  }
+};
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./components/homepage/Home";
+import HomePage from "./components/homepage/Home";
+import HomeLogin from "./components/login/HomeLogin";
 
 Vue.use(Router);
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: HomePage
+    },
+    {
+      path: "/login",
+      name: "homelogin",
+      component: HomeLogin
     }
   ]
 });
